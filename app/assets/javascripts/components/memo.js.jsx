1ef72aa92273ea9a80311742138b178598246465
itakurara <kaiamira26@gmail.com>
2016-04-17 22:42:55 +0900

    Add delete feature(not working properly)

--- app/assets/javascripts/components/memo.js.jsx
+++ app/assets/javascripts/components/memo.js.jsx
@@ -3,16 +3,33 @@ var MemoBox = React.createClass({
     return { data: [] }
   },
   handleNewText: function(editedData){
+    console.log('for ajax')
     $.ajax({
       url: this.props.url + '/' + editedData.id,
       datatype: 'json',
       type: 'PUT',
       data: editedData,
       success: function(data){
+        console.log('ajax success')
         this.setState({ data: data})
       }.bind(this),
       error: function(xhr, status, err){
-    }.bind(this)
+        console.error(this.props.url, status, err.toString())
+      }.bind(this)
+    });
+  },
+  handleDelete: function(deletedData){
+    $.ajax({
+      url: this.props.url + '/' + deletedData,
+      datatype: 'json',
+      type: 'DELETE',
+      data: deletedData,
+      success: function(data){
+        this.setState({ data: data })
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.error(this.props.url, status, err.toString())
+      }
     });
   },
   componentDidMount: function(){
@@ -29,7 +46,7 @@ var MemoBox = React.createClass({
   },
   render: function() {
     return (
-      <MemoList data={this.state.data} onChangeText={this.handleNewText} />
+      <MemoList data={this.state.data} onChangeText={this.handleNewText} onDeleteText={this.handleDelete} />
     );
   }
 });
@@ -37,13 +54,16 @@ var MemoList = React.createClass({
   handleNewText: function(editedData){
     this.props.onChangeText(editedData);
   },
+  handleDeletedText: function(deteledData){
+    this.props.onDeleteText(deteledData);
+  },
   render: function(){
     var colors = ['yellow', 'blue', 'pink', 'green'];
     var memoNodes = this.props.data.map(function(memo, i){
       var bgColor = colors[i%4];
       var classString = 'view view--' + bgColor;
       return (
-        <MemoView text={memo.content} id={memo.id} classString={classString} onChangeText={this.handleNewText} />
+        <MemoView text={memo.content} id={memo.id} classString={classString} onChangeText={this.handleNewText} onDeleteText={this.handleDeletedText} />
         );
     }.bind(this));
     return (
@@ -59,6 +79,9 @@ var MemoView = React.createClass({
       editing: false,
       classString: this.props.classString
     }
+  },
+  handleDeletedText: function(){
+    this.props.onDeleteText(this.props.id);
   },
   handleStateChange: function(text={}){
     if(this.state.editing){
@@ -79,7 +102,7 @@ var MemoView = React.createClass({
   render: function(){
     return (
       <div className={this.state.classString} >
-        <MemoText text={this.props.text} onClickEditBtn={this.handleStateChange} />
+        <MemoText text={this.props.text} onClickEditBtn={this.handleStateChange} onClickDeleteBtn={this.handleDeletedText} />
         <MemoForm text={this.props.text} onClickSubmitBtn={this.handleStateChange} />
       </div>
     );
@@ -89,12 +112,15 @@ var MemoText = React.createClass({
   handleEdit: function(){
     this.props.onClickEditBtn();
   },
+  handleDelete: function(){
+    this.props.onClickDeleteBtn();
+  },
   render: function(){
     return (
       <div className="memoText">
         <p>{this.props.text}</p>
         <input type="button" value="編集" className="memoText__btn" onClick={this.handleEdit} />
-        <input type="button" value="削除" className="memoText__btn" onClick={this.props.handleDelete} />
+        <input type="button" value="削除" className="memoText__btn" onClick={this.handleDelete} />
       </div>
     );
   }
@@ -103,15 +129,23 @@ var MemoForm = React.createClass({
   getInitialState: function(){
     return { text: this.props.text }
   },
+  componentWillReceiveProps: function(nextProps){
+    this.setState({ text: nextProps.text })
+    console.log('------------willreceiveProps-------------')
+    console.log(this.state.text);
+  },
   handleChange: function(e){
     this.setState({ text: e.target.value })
   },
   handleSubmit: function(){
     var text = this.state.text.trim();
     if(!text){ return }
+    console.log('--------------submit------------')
     this.props.onClickSubmitBtn(text);
   },
   render: function(){
+    console.log('fromMemoForm:render[state]' + this.state.text)
+    console.log('fromMemoForm:render[props]' + this.props.text)
     return (
       <div className="memoForm">
         <input type ="text" value={this.state.text} className="memoForm__txt" onChange={this.handleChange} />
--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,7 +3,7 @@ class MemosController < ApplicationController
   #include Slack
 
   skip_before_filter :verify_authenticity_token
-  before_filter :verify_slack_token, except: [:index, :update]
+  before_filter :verify_slack_token, except: [:index, :update, :destroy]
 
   def index
     @memos = Memo.all.order(created_at: :desc)
@@ -12,12 +12,19 @@ class MemosController < ApplicationController
 
   def create
     Memo.create!( content: params[:text] )
-    render status: :ok, text: "φ(.. )ﾒﾓｼﾃｵｺ"
+    render status: :ok, text: "φ(.. )ﾒﾓ"
   end
 
   def update
     memo = Memo.find(params[:id])
     memo.update(content: params[:text])
+    @memos = Memo.all.order(created_at: :desc)
+    render json: @memos
+  end
+
+  def destroy
+    memo = Memo.find(params[:id])
+    memo.destroy
     @memos = Memo.all.order(created_at: :desc)
     render json: @memos
   end
