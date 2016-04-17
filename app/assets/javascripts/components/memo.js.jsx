85edc10f7faf1796a5423e7be4f3827c9b227664
itakurara <kaiamira26@gmail.com>
2016-04-17 16:04:29 +0900

    Add editing feature

--- app/assets/javascripts/components/memo.js.jsx
+++ app/assets/javascripts/components/memo.js.jsx
@@ -2,6 +2,19 @@ var MemoBox = React.createClass({
   getInitialState: function(){
     return { data: [] }
   },
+  handleNewText: function(editedData){
+    $.ajax({
+      url: this.props.url + '/' + editedData.id,
+      datatype: 'json',
+      type: 'PUT',
+      data: editedData,
+      success: function(data){
+        this.setState({ data: data})
+      }.bind(this),
+      error: function(xhr, status, err){
+    }.bind(this)
+    });
+  },
   componentDidMount: function(){
     $.ajax({
       url: this.props.url,
@@ -16,20 +29,23 @@ var MemoBox = React.createClass({
   },
   render: function() {
     return (
-      <MemoList data={this.state.data} />
+      <MemoList data={this.state.data} onChangeText={this.handleNewText} />
     );
   }
 });
 var MemoList = React.createClass({
+  handleNewText: function(editedData){
+    this.props.onChangeText(editedData);
+  },
   render: function(){
     var colors = ['yellow', 'blue', 'pink', 'green'];
     var memoNodes = this.props.data.map(function(memo, i){
       var bgColor = colors[i%4];
       var classString = 'view view--' + bgColor;
       return (
-        <MemoView text={memo.content} classString={classString} />
+        <MemoView text={memo.content} id={memo.id} classString={classString} onChangeText={this.handleNewText} />
         );
-    });
+    }.bind(this));
     return (
       <div className="memoList" >
         {memoNodes}
@@ -38,14 +54,33 @@ var MemoList = React.createClass({
   }
 });
 var MemoView = React.createClass({
-  handleStateChange: function(){
-    // TODO
+  getInitialState: function(){
+    return {
+      editing: false,
+      classString: this.props.classString
+    }
+  },
+  handleStateChange: function(text={}){
+    if(this.state.editing){
+      this.setState({
+        editing: false,
+        classString: this.props.classString
+      });
+      if(text && text !== this.props.text){
+        this.props.onChangeText({ text: text, id: this.props.id });
+      }
+    }else{
+      this.setState({
+        editing: true,
+        classString: this.props.classString + " editing"
+      })
+    }
   },
   render: function(){
     return (
-      <div className={this.props.classString} >
+      <div className={this.state.classString} >
         <MemoText text={this.props.text} onClickEditBtn={this.handleStateChange} />
-        <MemoForm text={this.props.text} />
+        <MemoForm text={this.props.text} onClickSubmitBtn={this.handleStateChange} />
       </div>
     );
   }
@@ -59,15 +94,29 @@ var MemoText = React.createClass({
       <div className="memoText">
         <p>{this.props.text}</p>
         <input type="button" value="編集" className="memoText__btn" onClick={this.handleEdit} />
-        <input type="button" value="削除" className="memoText__btn" onClick={this.handleDelete} />
+        <input type="button" value="削除" className="memoText__btn" onClick={this.props.handleDelete} />
       </div>
     );
   }
 });
 var MemoForm = React.createClass({
+  getInitialState: function(){
+    return { text: this.props.text }
+  },
+  handleChange: function(e){
+    this.setState({ text: e.target.value })
+  },
+  handleSubmit: function(){
+    var text = this.state.text.trim();
+    if(!text){ return }
+    this.props.onClickSubmitBtn(text);
+  },
   render: function(){
     return (
-      <input type ="text" value={this.props.text} className="memoForm" />
+      <div className="memoForm">
+        <input type ="text" value={this.state.text} className="memoForm__btn" onChange={this.handleChange} />
+        <input type="button" value="OK!" onClick={this.handleSubmit} className="memoForm__btn" />
+      </div>
     );
   }
 });
--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,16 +3,23 @@ class MemosController < ApplicationController
   #include Slack
 
   skip_before_filter :verify_authenticity_token
-  before_filter :verify_slack_token, except: [:index]
+  before_filter :verify_slack_token, except: [:index, :update]
 
   def index
-    @memos = Memo.all
+    @memos = Memo.all.order(created_at: :desc)
     render json: @memos
   end
 
   def create
     Memo.create!( content: params[:text] )
     render status: :ok, text: "φ(.. )ﾒﾓｼﾃｵｺ"
+  end
+
+  def update
+    memo = Memo.find(params[:id])
+    memo.update(content: params[:text])
+    @memos = Memo.all.order(created_at: :desc)
+    render json: @memos
   end
 
   private
